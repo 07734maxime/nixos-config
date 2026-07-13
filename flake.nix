be7bb50862120885802eb5f868c34f668ff14456
07734maxime <07734maxime@proton.me>
2026-07-13 18:52:06 +0200

    add home manager

--- flake.nix
+++ flake.nix
@@ -8,15 +8,29 @@
       url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # On force HM à suivre la même version de nixpkgs que ton système
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+
   };
 
-  outputs = { self, nixpkgs, lanzaboote, ... }@inputs: {
+  outputs = { self, nixpkgs, lanzaboote, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; }; # Permet d'accéder aux inputs partout
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         lanzaboote.nixosModules.lanzaboote
+	home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.hello = import ./home.nix;
+        }
       ];
     };
   };
