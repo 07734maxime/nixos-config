bf73723ef62387e991cbea20fd74dac7ec4126fa
07734maxime <07734maxime@proton.me>
2026-07-25 16:21:28 +0200

    add matugen

--- home/matugen.nix
+++ home/matugen.nix
@@ -1,23 +1,27 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
-  programs.matugen = {
-    enable = true;
-    variant = "dark";
-    jsonFormat = "hex";
 
-    templates = {
-      kitty = {
-        input_path = ./templates/kitty-theme.conf;
-        output_path = "~/.config/kitty/theme.conf";
-      };
-      fuzzel = {
-        input_path = ./templates/fuzzel.ini;
-        output_path = "~/.config/fuzzel/fuzzel.ini";
-      };
-      niri = {
-        input_path = ./templates/niri-colors.kdl;
-        output_path = "~/.config/niri/colors.kdl";
-      };
-    };
-  };
+  home.packages = [ inputs.matugen.packages.${pkgs.system}.default ];
+
+  xdg.configFile."matugen/config.toml".text = ''
+    [config]
+    reload_apps = true
+
+    [templates.kitty]
+    input_path = "~/.config/matugen/templates/kitty-theme.conf"
+    output_path = "~/.config/kitty/theme.conf"
+
+    [templates.fuzzel]
+    input_path = "~/.config/matugen/templates/fuzzel.ini"
+    output_path = "~/.config/fuzzel/fuzzel.ini"
+
+    [templates.niri]
+    input_path = "~/.config/matugen/templates/niri-colors.kdl"
+    output_path = "~/.config/niri/colors.kdl"
+  '';
+
+
+  xdg.configFile."matugen/templates/kitty-theme.conf".source = ./templates/kitty-theme.conf;
+  xdg.configFile."matugen/templates/fuzzel.ini".source = ./templates/fuzzel.ini;
+	xdg.configFile."matugen/templates/niri-colors.kdl".source = ./templates/niri-colors.kdl;
 }
